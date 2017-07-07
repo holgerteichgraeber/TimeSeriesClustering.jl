da2037650430b1debdd234dd219fec1d095c98fc
Holger Teichgraeber <Holger Teichgraeber>
2017-07-07 09:34:43 -0700

    changes in dtw_dba_miniexample.jl - not working. Added matlab basic DBA code.

--- src/clust_algorithms/runfiles/dtw_dba_miniexample.jl
+++ src/clust_algorithms/runfiles/dtw_dba_miniexample.jl
@@ -1,4 +1,4 @@
-# imports 
+# imports
 using DataFrames
 using TimeWarp
 using TimeWarp.WarpPlots
@@ -26,21 +26,26 @@ else
   region_str = "GER_"
   region_data = normpath(joinpath(pwd(),"..","..","..","data","el_prices","GER_2015_elPrice.txt"))
 end
-data_orig = Array(readtable(region_data, separator = '\t', header = false)) 
-data_orig_daily = reshape(data_orig,24,365) 
+data_orig = Array(readtable(region_data, separator = '\t', header = false))
+data_orig_daily = reshape(data_orig,24,365)
 
 println("data loaded")
 
 seq1 = Sequence(data_orig_daily[:,1])
 seq2 = Sequence(data_orig_daily[:,2])
+seq = Sequence(data_orig_daily[:,1:3])
 
 # test dtw
 D = dtw(seq1,seq2)
 println("D ",D)
- 
+E = fastdtw(seq1,seq2,24)
+println("E ",E)
+
+#dtwplot(seq1,seq2)
+#show()
  # test dba
- #avg, result = dba(data)
- #println(avg)
+avg, result = dba(seq,ClassicDTW(),iterations=1)
+println(avg)
 
 
 
---
+++ src/clust_algorithms/runfiles/runfile_DBA.m
@@ -0,0 +1,77 @@
+% Runscript Matlab DBA
+% 
+% Holger Teichgraeber
+% July 2017
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+close all
+clear all
+clc
+
+%% Test 1 - Provided and adapted from original matlab file
+
+n_seq =5;
+length_seq=24;
+
+sequences = {};
+sequences{n_seq}=[];
+mean_euc = zeros(length_seq,1);
+for i=1:n_seq
+    length = length_seq;
+    sequences{i}=rand(1,length);
+    mean_euc = mean_euc + sequences{i}';
+end
+mean_dba=DBA(sequences);
+mean_euc=mean_euc/n_seq;
+
+%% plot test 1
+figure()
+hold all
+grey=0.3;
+for i=1:n_seq
+    plot(sequences{i},'Color',[grey grey grey])
+    plot(mean_dba,'r')
+    plot(mean_euc,'b')
+end
+title('Test 1 - random data')
+plotfixer
+
+%% Test 2 - 
+
+ca_data = load('ca_2015_orig.txt');
+ger_data = load('GER_2015_elPrice.txt');
+
+% INPUT
+data = ca_data;
+ind = linspace(1,10,10); % indices of data considered
+
+data = reshape(data',24,365);
+data = data(:,ind);
+
+days = {};
+days{size(data,2)} = [];
+for i=1:size(data,2)
+    days{i} = data(:,i);
+end
+mean_dba_data=DBA(days);
+mean_euc_data=mean(data,2);
+
+
+%% plot test 2
+figure()
+hold all
+grey=0.3;
+for i=1:size(data,2)
+    plot(days{i},'Color',[grey grey grey])
+    plot(mean_dba_data,'r')
+    plot(mean_euc_data,'b')
+end
+title('Test 2 ')
+plotfixer
+
+
+
+
+
+
