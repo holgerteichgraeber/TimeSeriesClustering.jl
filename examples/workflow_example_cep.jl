b6ac1ea938854f263ed26e840066a46f0b23fe5a
YoungFaithful <kuepper@stanford.edu>
2018-12-10 10:28:30 -0800

    Transmission finished

--- examples/workflow_example_cep.jl
+++ examples/workflow_example_cep.jl
@@ -11,20 +11,7 @@ ts_input_data, = load_timeseries_data("CEP", "GER_1";K=365, T=24) #CEP
 cep_input_data_GER=load_cep_data("GER_1")
 
  # run clustering
-ts_clust_res = run_clust(ts_input_data;method="kmeans",representation="centroid",n_init=1,n_clust=10) # default k-means
+ts_clust_res = run_clust(ts_input_data;method="kmeans",representation="centroid",n_init=10,n_clust=5) # default k-means
 
  # optimization
-model = run_opt(ts_clust_res.best_results,cep_input_data_GER;solver=GurobiSolver(), co2_limit=100, storage=true, interstorage=true, best_ids=ts_clust_res.best_ids)
-
-#using Plots
-gr(size=(1000,1000),width=2)
-plots=[]
- push!(plots,plot(get_cep_variable_value(model.variables["INTRASTOR"],["el","bat",:,:,"germany"]),legend=false))
- push!(plots,plot(get_cep_variable_value(model.variables["GEN"],["el","bat",:,:,"germany"]),legend=false))
- try
-     push!(plots,plot(get_cep_variable_value(model.variables["INTERSTOR"],["el","bat",:,"germany"])))
- catch
- end
-
-pall=plot(plots..., layout=grid(length(plots),1))
-savefig(pall, "interstorage.svg")
+model = run_opt(ts_clust_res.best_results, cep_input_data_GER;solver=GurobiSolver(), co2_limit=250, existing_infrastructure=false)
