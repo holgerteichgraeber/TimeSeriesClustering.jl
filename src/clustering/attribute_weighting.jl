e4890ea3fbc03c9d7de3fd11e9fad7196b3f1f21
elias.kuepper <elias.kuepper@rwth-aachen.de>
2019-04-29 13:54:56 -0700

    AbstractFloat

--- src/clustering/attribute_weighting.jl
+++ src/clustering/attribute_weighting.jl
@@ -1,10 +1,10 @@
 """
-function attribute_weighting(data::ClustData,attribute_weights::Dict{String,Float})
+function attribute_weighting(data::ClustData,attribute_weights::Dict{String,AbstractFloat})
 
 apply the different attribute weights based on the dictionary entry for each tech or exact name
 """
 function attribute_weighting(data::ClustData,
-                              attribute_weights::Dict{String,Float}
+                              attribute_weights::Dict{String,AbstractFloat}
                               )
   for name in keys(data.data)
     tech=split(name,"-")[1]
