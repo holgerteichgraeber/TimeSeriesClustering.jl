c6a5d19d6b6b162a40b29157eba898c743b43557
Holger Teichgraeber <holger@teichgr.de>
2019-06-27 13:32:40 -0700

    run_clust n_clust=1,5,K tests  - passing

--- test/reference_generation/run_clust.jl
+++ test/reference_generation/run_clust.jl
@@ -1,7 +1,9 @@
 using ClustForOpt
+using JLD2
 
 reference_results = Dict{String,Any}()
 
+Random.seed!(1111)
 for data in ["CEP_GER1","CEP_GER18"]
     ts_input_data = load_timeseries_data(Symbol(data))
     #mr: method, representation, n_init
@@ -13,6 +15,7 @@ for data in ["CEP_GER1","CEP_GER18"]
     ["hierarchical","medoid",1]]
     # default
     for (method,repr,n_init) in mr
+        Random.seed!(1111)
         try
             reference_results["$data-$method-$repr-default"] = run_clust(ts_input_data;method=method,representation=repr,n_clust=5,n_init=n_init)
         catch
@@ -22,22 +25,28 @@ for data in ["CEP_GER1","CEP_GER18"]
     end
     # n_clust=1
     for (method,repr,n_init) in mr
+        Random.seed!(1111)
         try
-            reference_results["$data-$method-$repr-default"] = run_clust(ts_input_data;method=method,representation=repr,n_clust=1,n_init=n_init)
+            reference_results["$data-$method-$repr-n_clust1"] = run_clust(ts_input_data;method=method,representation=repr,n_clust=1,n_init=n_init)
         catch
-            reference_results["$data-$method-$repr-default"] = "not defined"
-            println("$data-$method-$repr-default not defined")
+            reference_results["$data-$method-$repr-n_clust1"] = "not defined"
+            println("$data-$method-$repr-n_clust1 not defined")
         end
     end
     # n_clust = N
     for (method,repr,n_init) in mr
+        Random.seed!(1111)
         try
-            reference_results["$data-$method-$repr-default"] = run_clust(ts_input_data;method=method,representation=repr,n_clust=ts_input_data.K,n_init=n_init)
+            reference_results["$data-$method-$repr-n_clustK"] = run_clust(ts_input_data;method=method,representation=repr,n_clust=ts_input_data.K,n_init=n_init)
         catch
-            reference_results["$data-$method-$repr-default"] = "not defined"
-            println("$data-$method-$repr-default not defined")
+            reference_results["$data-$method-$repr-n_clustK"] = "not defined"
+            println("$data-$method-$repr-n_clustK not defined")
         end
     end
+#    mr_kmexact = [["kmedoids_exact","centroid",1],
+#    for (method,repr,n_init) in mr_kmexact
+
+#    end
 end
 
 
