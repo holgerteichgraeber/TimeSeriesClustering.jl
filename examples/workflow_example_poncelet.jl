2c9bcda320b1c232415ad2427868b074430e23df
lgo <lgo@wip.tu-berlin.de>
2019-11-11 20:35:11 +0100

    included cbc solver in example for poncelet

--- examples/workflow_example_poncelet.jl
+++ examples/workflow_example_poncelet.jl
@@ -2,7 +2,12 @@ using TimeSeriesClustering
 
 ts_input_data = load_timeseries_data(:CEP_GER1)
 
-# when using kmedoids-exact, one needs to supply the optimizer. Make sure the optimizer is added through Pkg.add()
+# when using poncelet, one needs to supply the optimizer. Make sure the optimizer is added through Pkg.add()
+
+using Cbc
+optimizer = Cbc.Optimizer
+out = run_clust(ts_input_data;method="poncelet",n_init=5,ponc_optimizer=optimizer)
+
 
 using Gurobi
 optimizer = Gurobi.Optimizer
